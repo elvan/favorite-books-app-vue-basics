2107f2bbdfc2672dc9bd81fe1c3b447ab15f7df2
Elvan H <elvanhd@gmail.com>
2022-01-28 06:06:16 +0700

    attribute binding

--- app.js
+++ app.js
@@ -9,26 +9,36 @@ const app = Vue.createApp({
           title: 'In Search of Lost Time',
           author: 'Marcel Proust',
           year: 1913,
+          imageUrl:
+            'https://d3i5mgdwi2ze58.cloudfront.net/7hqv6ddaqv363p4hadx6lymotow1',
         },
         {
           title: 'Ulysses',
           author: 'James Joyce',
           year: 1922,
+          imageUrl:
+            'https://d3i5mgdwi2ze58.cloudfront.net/f7nkbyqfsnrrlct3hs01jkrz2vdi',
         },
         {
           title: 'Don Quixote',
           author: 'Miguel de Cervantes',
           year: 1605,
+          imageUrl:
+            'https://d3i5mgdwi2ze58.cloudfront.net/y4332t5mbkhbhkrjftg4jksj4tzw',
         },
         {
           title: 'One Hundred Years of Solitude',
           author: 'Gabriel Garcia Marquez',
           year: 1967,
+          imageUrl:
+            'https://d3i5mgdwi2ze58.cloudfront.net/j7koelfcv0va5zky4x1ccmpsgcsb',
         },
         {
           title: 'The Great Gatsby',
           author: 'F. Scott Fitzgerald',
           year: 1925,
+          imageUrl:
+            'https://d3i5mgdwi2ze58.cloudfront.net/m8sdet02cc7mdl8l6jow8cpl7co2',
         },
       ],
       offsetX: 0,
@@ -37,33 +47,8 @@ const app = Vue.createApp({
   },
 
   methods: {
-    increase() {
-      this.year++;
-    },
-
-    decrease() {
-      this.year--;
-    },
-
-    changeTitle(title) {
-      this.title = title;
-    },
-
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
-    },
-
-    handleEvent($event, data) {
-      console.log($event);
-
-      if (data) {
-        console.log(data);
-      }
-    },
-
-    handleMouseMove($event) {
-      this.offsetX = $event.offsetX;
-      this.offsetY = $event.offsetY;
     },
   },
 });
