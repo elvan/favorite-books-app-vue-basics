7645522cc82932adc1cacffdb52005c44aef2f37
Elvan H <elvanhd@gmail.com>
2022-01-28 06:14:16 +0700

    dynamic classes

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = Vue.createApp({
           year: 1913,
           imageUrl:
             'https://d3i5mgdwi2ze58.cloudfront.net/7hqv6ddaqv363p4hadx6lymotow1',
+          isFavorite: true,
         },
         {
           title: 'Ulysses',
@@ -18,6 +19,7 @@ const app = Vue.createApp({
           year: 1922,
           imageUrl:
             'https://d3i5mgdwi2ze58.cloudfront.net/f7nkbyqfsnrrlct3hs01jkrz2vdi',
+          isFavorite: false,
         },
         {
           title: 'Don Quixote',
@@ -25,6 +27,7 @@ const app = Vue.createApp({
           year: 1605,
           imageUrl:
             'https://d3i5mgdwi2ze58.cloudfront.net/y4332t5mbkhbhkrjftg4jksj4tzw',
+          isFavorite: false,
         },
         {
           title: 'One Hundred Years of Solitude',
@@ -32,6 +35,7 @@ const app = Vue.createApp({
           year: 1967,
           imageUrl:
             'https://d3i5mgdwi2ze58.cloudfront.net/j7koelfcv0va5zky4x1ccmpsgcsb',
+          isFavorite: false,
         },
         {
           title: 'The Great Gatsby',
@@ -39,6 +43,7 @@ const app = Vue.createApp({
           year: 1925,
           imageUrl:
             'https://d3i5mgdwi2ze58.cloudfront.net/m8sdet02cc7mdl8l6jow8cpl7co2',
+          isFavorite: true,
         },
       ],
       offsetX: 0,
